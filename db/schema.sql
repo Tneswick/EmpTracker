5bdbc3c9656bdf635c26ac470ba6a6a0a833a90b
Deck Stream <tneswick2@verizon.net>
2022-04-16 13:14:52 -0700

    schema

--- db/schema.sql
+++ db/schema.sql
@@ -3,14 +3,10 @@ CREATE DATABASE company_db;
 
 USE company_db;
 
-DROP TABLE IF EXISTS department;
-DROP TABLE IF EXISTS role;
-DROP TABLE IF EXISTS employee;
-
 CREATE TABLE department(
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
-)
+);
 
 CREATE TABLE role(
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -18,7 +14,7 @@ CREATE TABLE role(
     salary DECIMAL(6,2) NOT NULL,
     department_id INT NOT NULL,
     CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
-)
+);
 
 CREATE TABLE employee(
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -27,11 +23,9 @@ CREATE TABLE employee(
     role_id INT NOT NULL,
     manager_id INT,
     CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
-    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE NULL
-)
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
 
--- Check on constraints particularly referencing another coloumn in the same table
+-- Check on constraints particularly referencing another column in the same table
 
--- Ask about the need for a server for this challenge if it's not being deployed to heroku and will be exemplified by a video
-
--- Ask about 
+-- Ask about "ON DELETE" in depth
